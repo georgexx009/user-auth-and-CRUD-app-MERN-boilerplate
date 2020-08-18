67b8554813dce698ee5b5046578ebd5de87fc9e9
Emmanuel <georgexx009@hotmail.com>
2020-08-17 20:18:17 -0500

    delete redux middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from '../reducers';
 import { initialState } from './initialState';
-import apiMiddleware from '../redux-middleware';
-import thunkMiddleware from 'redux-thunk';
 
-export const store = createStore(
-  rootReducer,
-  initialState,
-  applyMiddleware(apiMiddleware, thunkMiddleware)
-);
+export const store = createStore(rootReducer, initialState);
