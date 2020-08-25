637e3e8bec01fd7d69d3b79f6419b461715952ca
Emmanuel <georgexx009@hotmail.com>
2020-08-25 13:43:45 -0500

    app: fix prototype pollution cause by minimist old version

--- src/components/container/pubsContainer/utils.js
+++
@@ -1,8 +0,0 @@
-import { urlServer } from '../../../../constants';
-import { updatePubs } from '../../../actions';
-
-export const fetchPubsObj = {
-  url: `${urlServer}/publications/filterPubs`,
-  method: 'POST',
-  onSuccess: updatePubs,
-};
