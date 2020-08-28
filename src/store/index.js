9a118c88eed94fcee94d8911169839baf3a6c51a
Emmanuel <georgexx009@hotmail.com>
2020-08-27 20:23:26 -0500

    final refactor before start documenting

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { rootReducer } from '../reducers';
 import { initialState } from './initialState';
 
 export const store = createStore(
   rootReducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // line for apply redux debug on Firefox
 );
--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,31 +1,4 @@
 export const initialState = {
-  cardInfo: [
-    {
-      title: 'Madara Uchiha',
-      content:
-        'Madara Uchiha (うちはマダラ, Uchiha Madara) fue un legendario shinobi,co-fundador de Konoha y líder del Clan Uchiha durante su tiempo. Además, fue la pasada Reencarnación de Indra Ōtsutsuki.',
-    },
-  ],
-  pubsInfo: [
-    {
-      username: 'Sasuke Uchiha',
-      content:
-        'Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) es el deuteragonista de la serie y uno de los supervivientes del Clan Uchiha.',
-    },
-    {
-      username: 'Sasuke Uchiha',
-      content:
-        'Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) es el deuteragonista de la serie y uno de los supervivientes del Clan Uchiha.',
-    },
-  ],
-  modalData: {
-    display: false,
-    cardInfo: {
-      title: 'test',
-      content: 'test...',
-    },
-  },
-
   snackbarData: {
     show: false,
     label: '',
